954bd48cb668a0919f4406d61882d3d5ea15e65b
kamil <kamil.bizon@protonmail.com>
2019-01-31 12:54:44 +0100

    Exercise 1 from chapter 8

---
+++ chapter-8/exercise-1.js
@@ -0,0 +1,36 @@
+/* Exercise 1 from chapter 8 - Retry
+
+Say you have a function primitiveMultiply that in 20 percent of cases multiplies two numbers and in the other 80 percent of cases raises an exception of type MultiplicatorUnitFailure. Write a function that wraps this clunky function and just keeps trying until a call succeeds, after which it returns the result.
+
+Make sure you handle only the exceptions you are trying to handle.
+
+*/
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+// Recursively, can overflow stack when infinite loop
+function reliableMultiply(a, b) {
+  try {
+    return primitiveMultiply(a, b);
+  } catch (e) {
+    if (e instanceof MultiplicatorUnitFailure) {
+      console.log('Retrying...');
+      return reliableMultiply(a, b);
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+/*
+  Retrying...
+  Retrying...
+  64
+*/
