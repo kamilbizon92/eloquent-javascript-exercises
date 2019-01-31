f8f025452e77601cd192045d7eae736f193fee62
kamil <kamil.bizon@protonmail.com>
2019-01-31 13:23:14 +0100

    Exercise 2 from chapter 8

---
+++ chapter-8/exercise-2.js
@@ -0,0 +1,49 @@
+/* Exercise 2 from chapter 8 - The locked box
+
+It is a box with a lock. There is an array in the box, but you can get at it only when the box is unlocked. Directly accessing the private _content property is forbidden.
+
+Write a function called withBoxUnlocked that takes a function value as argument, unlocks the box, runs the function, and then ensures that the box is locked again before returning, regardless of whether the argument function returned normally or threw an exception.
+
+For extra points, make sure that if you call withBoxUnlocked when the box is already unlocked, the box stays unlocked.
+
+*/
+
+const box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  if (!box.locked) {
+    return body();
+  } else {
+    // If box is locked, unlock, execute function and lock it
+    box.unlock();
+    try {
+      return body();
+    } finally {
+      box.lock();
+    }
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+
+console.log(box.locked);
+// → true
