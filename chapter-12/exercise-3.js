674bf28a3a441c17e8e3a730a1b1ea79110170f6
kamil <kamil.bizon@protonmail.com>
2019-02-04 21:12:10 +0100

    Exercise 3 from chapter 12

---
+++ chapter-12/exercise-3.js
@@ -0,0 +1,21 @@
+/* Exercise 3 from chapter 12 - Comments
+
+It would be nice if we could write comments in Egg. For example, whenever we find a hash sign (#), we could treat the rest of the line as a comment and ignore it, similar to // in JavaScript.
+
+We do not have to make any big changes to the parser to support this. We can simply change skipSpace to skip comments as if they are whitespace so that all the points where skipSpace is called will now also skip comments. Make this change.
+
+*/
+
+function skipSpace(string) {
+  // Return array if match
+  let first = string.match(/^(\s|#.*)*/);
+  return string.slice(first[0].length);
+}
+
+console.log(parse("# hello\nx"));
+// → {type: "word", name: "x"}
+
+console.log(parse("a # one\n   # two\n()"));
+// → {type: "apply",
+//    operator: {type: "word", name: "a"},
+//    args: []}
